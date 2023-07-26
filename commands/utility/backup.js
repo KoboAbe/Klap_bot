6685d46a8692740b54e422fbf2a21ac2296b88be
KoboAbe <javasmu@gmail.com>
2023-07-25 19:24:23 -0500

    Updates on backup command

--- commands/utility/backup.js
+++ commands/utility/backup.js
@@ -1,33 +1,85 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+
+const downloadsDir = './downloads';
+if (!fs.existsSync(downloadsDir)) {
+	fs.mkdirSync(downloadsDir);
+}
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('backup')
-		.setDescription('Hace un backup del chat actual.'),
+		.setDescription('Descarga el historial del chat (mensajes, imágenes y videos).'),
 	async execute(interaction) {
 		const channel = interaction.channel;
 		if (!channel) return;
 
-		const messages = await channel.messages.fetch({ limit: 100 });
+		let allMessages = [];
+		let lastMessageId;
 
-		let backupContent = 'Backup del chat:\n\n';
+		// Recopilamos todos los mensajes del canal de forma paginada
+		do {
+			const options = { limit: 100 };
+			if (lastMessageId) options.before = lastMessageId;
 
-		messages.sort((a, b) => a.createdTimestamp - b.createdTimestamp);
+			const messages = await channel.messages.fetch(options);
+			const filteredMessages = messages.filter((message) => !message.author.bot);
 
-		messages.forEach((message) => {
+			allMessages.push(...filteredMessages.values());
+			lastMessageId = messages.last()?.id;
+		} while (lastMessageId);
+
+		let backupContent = 'Historial del chat:\n\n';
+
+		// Ordenamos los mensajes por fecha de publicación
+		allMessages.sort((a, b) => a.createdTimestamp - b.createdTimestamp);
+
+		// Iteramos a través de todos los mensajes
+		allMessages.forEach((message) => {
 			backupContent += `Fecha: ${message.createdAt}\nAutor: ${message.author.tag}\nContenido: ${message.content}\n\n`;
 
-			if (message.embeds.length > 0) {
-				message.embeds.forEach((embed, index) => {
-					backupContent += `Embed ${index + 1}:\n${JSON.stringify(embed, null, 2)}\n\n`;
+			// Si el mensaje contiene adjuntos (imágenes o videos), los descargamos
+			if (message.attachments.size > 0) {
+				message.attachments.forEach(async (attachment) => {
+					const fileName = `${message.id}_${attachment.name}`;
+
+					await downloadAttachment(attachment.url, fileName);
+					backupContent += `Adjunto: ${attachment.url}\n\n`;
 				});
 			}
 		});
 
-		// Envía el backup al usuario que solicitó el comando
-		interaction.user.send('Aquí está el backup del chat:', { files: [{ name: 'backup.txt', attachment: Buffer.from(backupContent) }] });
+		// Guardamos el historial del chat en un archivo de texto
+		fs.writeFileSync('./downloads/chat_history.txt', backupContent);
 
-		// Opcionalmente, podrías enviar también el backup como mensaje en el canal actual
-		// interaction.reply('Aquí está el backup del chat:', { files: [{ name: 'backup.txt', attachment: Buffer.from(backupContent) }] });
+		// Enviamos el archivo de texto al usuario que solicitó el comando
+		interaction.user.send('Aquí está el historial del chat:', {
+			files: [{ name: 'chat_history.txt', attachment: './downloads/chat_history.txt' }],
+		});
+
+		// Opcionalmente, podrías enviar también el historial del chat como mensaje en el canal actual
+		// interaction.reply('Aquí está el historial del chat:', { files: [{ name: 'chat_history.txt', attachment: './downloads/chat_history.txt' }] });
 	},
 };
+
+// Función para descargar un archivo adjunto
+function downloadAttachment(url, filename) {
+	return new Promise((resolve, reject) => {
+		const fileStream = fs.createWriteStream(`./downloads/${filename}`);
+		const https = require('https');
+
+		https.get(url, (response) => {
+			response.pipe(fileStream);
+
+			fileStream.on('finish', () => {
+				fileStream.close();
+				resolve();
+			});
+
+			fileStream.on('error', (error) => {
+				fs.unlinkSync(`./downloads/${filename}`);
+				reject(error);
+			});
+		});
+	});
+}
